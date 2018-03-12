1d283b7b4d3395a436525812540f9cca26a5bec0
Carlota Bindner <cev.bindner@gmail.com>
2018-03-12 18:39:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,35 +3,35 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_page)
+  def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    index_page.css("div.roster-cards-container").each { |card|
-      card.css(".student-card a").each { |student|
-        student_name = student.css('.student-name').text
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
         student_location = student.css('.student-location').text
-        student_profile_url = "#{student.attr('href')}"
-        students << {name: student_name, location: student_location, profile_url: student_profile_url}
-      }
-    }
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
     students
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_slug)
     student = {}
-    profile_page = Nokogiri::HTML(open("profile_url"))
-    links = profile_page.css("div.social-icon-container").children.css("a").map { |icon| icon.attribute('href').value }
-    links.each { |link|
-      if link.include?("twitter")
-        student[:twitter] = link
-      elsif link.include?("linkedin")
+    profile_page = Nokogiri::HTML(open(profile_slug))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
         student[:linkedin] = link
       elsif link.include?("github")
         student[:github] = link
-      else link.include?("blog")
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
         student[:blog] = link
       end
-    }
+    end
     student[:profile_quote] = profile_page.css(".profile-quote").text
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
   end
